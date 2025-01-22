806a73fba1651cdff94b0079dd0ffbf380e7dba1
MrDomoArigato98 <domdob98@gmail.com>
2025-01-22 09:59:05 +0000

    Creates 16x16 grid

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 const grid = document.querySelector("#grid")
 
-const boxContainer = document.createElement("div")
-boxContainer.classList.add("boxContainer")
+
 
 for (let y = 0; y < 16; y++) {
+    //Creates box
     const box = document.createElement("div")
     box.classList.add("box");
-    grid.appendChild(box)
+    //Adds box into a div going across X axis
+    const boxContainer = document.createElement("div")
+    boxContainer.classList.add("boxContainer")
+    //Adds box into box container, then adds container into grid.
+    boxContainer.appendChild(box)
+    grid.appendChild(boxContainer)
     
-    // for (let x = 0; x < 16; x++) {
-    //     const box = document.createElement("div")
-    //     box.classList.add("box");
-    //     grid.appendChild(box)
-    // }
+    for (let x = 0; x < 15; x++) {
+        const box = document.createElement("div")
+        box.classList.add("box");
+        //Adds box into a div going across X axis
+        const boxContainer = document.createElement("div")
+        boxContainer.classList.add("boxContainer")
+        //Adds box into box container, then adds container into grid.
+        boxContainer.appendChild(box)
+        grid.appendChild(boxContainer)
+     }
 }
