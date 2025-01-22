21b2be9a4e5a0ed07e3f20c444f4d3155ee8632a
MrDomoArigato98 <domdob98@gmail.com>
2025-01-22 10:42:49 +0000

    Removes bugs with box creation
    Previously the boxes were off, and were inaccurate. It also didn't allow too create dynamic sized grids.
    I had some unnecessary and extra Y-Axis div creations that were wrong, but somehow still worked (it shouldn't).
    That's removed. Now I can add dynamic input

--- js/script.js
+++ js/script.js
@@ -1,35 +1,46 @@
 const grid = document.querySelector("#grid")
 
-
+let xAxisUserInput = 0;
+let yAxisUserInput = 0;
 
 for (let y = 0; y < 16; y++) {
-    //Creates box
-    const box = document.createElement("div")
-    box.classList.add("box");
-    //Adds box into a div going across X axis
-    const boxContainer = document.createElement("div")
-    boxContainer.classList.add("boxContainer")
-    //Adds box into box container, then adds container into grid.
-    boxContainer.appendChild(box)
-    grid.appendChild(boxContainer)
+    //Create row div
+    const row = document.createElement("div")
+    row.classList.add("row")
+    grid.appendChild(row)
     
-    for (let x = 0; x < 15; x++) {
+    for (let x = 0; x < 16; x++) {
+        //Create box within row div
         const box = document.createElement("div")
         box.classList.add("box");
         //Adds box into a div going across X axis
-        const boxContainer = document.createElement("div")
-        boxContainer.classList.add("boxContainer")
+        const row = document.createElement("div")
+        row.classList.add("row")
         //Adds box into box container, then adds container into grid.
-        boxContainer.appendChild(box)
-        grid.appendChild(boxContainer)
+        row.appendChild(box)
+        grid.appendChild(row)
      }
 }
 
+// function createGridRow(xAxisUserInput){
+//     for (let x = 0; x < xAxisUserInput; x++) {
+
+//     }
+
+// }
+
+// function createGridColumn(yAxisUserInput){
+//     for (let y = 0; y < yAxisUserInput; y++) {
+
+//     }
+
+// }
+
 //Get all elements with class box
-let divArray = document.querySelectorAll(".box");
-console.log(divArray);
-divArray.forEach(function (elem) {
-    elem.addEventListener("mouseover", function() {
-        elem.classList.add("hover");
-    });  
-})
+// let divArray = document.querySelectorAll(".box");
+// console.log(divArray);
+// divArray.forEach(function (elem) {
+//     elem.addEventListener("mouseover", function() {
+//         elem.classList.add("hover");
+//     });  
+// })
