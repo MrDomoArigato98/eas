8ea2f1cd6264c6068352478553f4dd1de08530bf
MrDomoArigato98 <domdob98@gmail.com>
2025-01-22 10:56:53 +0000

    Refactors for loop for grid creation into a fn
    Created a function for making the grid instead of a for-loop. Makes it more readable and callable when I need it with the parameters

--- js/script.js
+++ js/script.js
@@ -1,46 +1,47 @@
 const grid = document.querySelector("#grid")
 
-let xAxisUserInput = 0;
-let yAxisUserInput = 0;
+let xAxisUserInput = 16;
+let yAxisUserInput = 16;
 
-for (let y = 0; y < 16; y++) {
-    //Create row div
-    const row = document.createElement("div")
-    row.classList.add("row")
-    grid.appendChild(row)
+// for (let y = 0; y < 16; y++) {
+//     //Create row div
+//     const row = document.createElement("div") 
+//     row.classList.add("row")
+//     grid.appendChild(row)
     
-    for (let x = 0; x < 16; x++) {
-        //Create box within row div
-        const box = document.createElement("div")
-        box.classList.add("box");
-        //Adds box into a div going across X axis
-        const row = document.createElement("div")
+//     for (let x = 0; x < 16; x++) {
+//         //Create box within row div
+//         const box = document.createElement("div")
+//         box.classList.add("box");
+//         //Adds box into box container, then adds container into grid.
+//         row.appendChild(box)
+//      }
+// }
+
+const createGrid = (xAxisUserInput,yAxisUserInput) => {
+    for (let y = 0; y < yAxisUserInput; y++) {
+        //Create row
+        const row = document.createElement("div") 
         row.classList.add("row")
-        //Adds box into box container, then adds container into grid.
-        row.appendChild(box)
         grid.appendChild(row)
-     }
+        
+        for (let x = 0; x < xAxisUserInput; x++) {
+            //Create boxes within row
+            const box = document.createElement("div")
+            box.classList.add("box");
+            //Adds box into row
+            row.appendChild(box)
+         }
+    }
+
 }
-
-// function createGridRow(xAxisUserInput){
-//     for (let x = 0; x < xAxisUserInput; x++) {
-
-//     }
-
-// }
-
-// function createGridColumn(yAxisUserInput){
-//     for (let y = 0; y < yAxisUserInput; y++) {
-
-//     }
-
-// }
+createGrid(xAxisUserInput,yAxisUserInput);
 
 //Get all elements with class box
-// let divArray = document.querySelectorAll(".box");
-// console.log(divArray);
-// divArray.forEach(function (elem) {
-//     elem.addEventListener("mouseover", function() {
-//         elem.classList.add("hover");
-//     });  
-// })
+let divArray = document.querySelectorAll(".box");
+console.log(divArray);
+divArray.forEach(function (elem) {
+    elem.addEventListener("mouseover", function() {
+        elem.classList.add("hover");
+    });  
+})
