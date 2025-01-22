defbcec35521a95cedaa1fda6ee9bb339cf830d2
MrDomoArigato98 <domdob98@gmail.com>
2025-01-22 09:51:03 +0000

    Creates centered 16 boxes on X axis

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const grid = document.querySelector("#grid")
 
-for (let i = 0; i < 16; i++) {
+const boxContainer = document.createElement("div")
+boxContainer.classList.add("boxContainer")
+
+for (let y = 0; y < 16; y++) {
     const box = document.createElement("div")
     box.classList.add("box");
     grid.appendChild(box)
     
-    for (let j = 0; j < 16; j++) {
-        const box = document.createElement("div")
-        box.classList.add("box");
-        grid.appendChild(box)
-    }
+    // for (let x = 0; x < 16; x++) {
+    //     const box = document.createElement("div")
+    //     box.classList.add("box");
+    //     grid.appendChild(box)
+    // }
 }
